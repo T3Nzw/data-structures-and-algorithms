258f4220525cc169e00cc721ada6921933114492
T3Nzw <t3nszw@gmail.com>
2025-12-03 03:56:20 +0200

    todo + cleaned up version of height

--- 07. Binary Tree/BinaryTreeUtil.hpp
+++ 07. Binary Tree/BinaryTreeUtil.hpp
@@ -3,6 +3,8 @@
 
 #include <algorithm>
 
+// TODO: поне 02, 03, 06, 07 от задачите
+
 // както говорихме на семинара, BinaryTreeType е "типов конструктор",
 // т.е. за да го използваме на мястото на тип, трябва да го
 // инстанцираме с някакъв конкретен тип (напр. BinaryTreeType<T>)
@@ -10,14 +12,13 @@ template<typename T, template<typename> class BinaryTreeType>
 class BinaryTreeUtil {
 public:
   static int height(BinaryTreeType<T> const &tree) {
-    if (tree.empty()) return -1;
-    return std::max(height(tree.root().left()),
-                    height(tree.root().right()));
+    return height(tree.root());
   }
 
 private:
   static int height(typename BinaryTreeType<T>::position pos) {
-    if (!pos.valid()) return 0;
+    if (!pos.valid()) return -1;
+    if (!pos.left().valid() && !pos.right().valid()) return 0;
     return 1 + std::max(height(pos.left()), height(pos.right()));
   }
 
