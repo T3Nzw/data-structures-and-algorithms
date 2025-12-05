04628895b73772f82547b896174e03110a9400a9
T3Nzw <t3nszw@gmail.com>
2025-12-05 14:28:21 +0200

    add solutions

--- 07. Binary Tree/BinaryTreeUtil.hpp
+++ 07. Binary Tree/BinaryTreeUtil.hpp
@@ -1,25 +1,155 @@
 #ifndef __BINARY_TREE_UTIL_HPP
 #define __BINARY_TREE_UTIL_HPP
 
+#include <queue>
+#include <vector>
+#include <cstddef>
 #include <algorithm>
+#include <iostream>
 
-// TODO: поне 02, 03, 06, 07 от задачите
-
-// както говорихме на семинара, BinaryTreeType е "типов конструктор",
-// т.е. за да го използваме на мястото на тип, трябва да го
-// инстанцираме с някакъв конкретен тип (напр. BinaryTreeType<T>)
 template<typename T, template<typename> class BinaryTreeType>
 class BinaryTreeUtil {
+  using position = typename BinaryTreeType<T>::position;
 public:
-  static int height(BinaryTreeType<T> const &tree) {
-    return height(tree.root());
+  static long long height(BinaryTreeType<T> const &t) {
+    return height(t.root());
+  }
+
+  static long long diameter(BinaryTreeType<T> const &t) {
+    return diameter(t.root());
+ }
+
+  static void serialise(BinaryTreeType<T> const &t, std::ostream& os=std::cout) {
+    serialise(t.root(), "", false, os);
+  }
+
+  static std::vector<T> inorder(BinaryTreeType<T> const &t) {
+    std::vector<T> result;
+    inorder(t.root(), result);
+    return result;
+  }
+
+  static std::vector<T> preorder(BinaryTreeType<T> const &t) {
+    std::vector<T> result;
+    preorder(t.root(), result);
+    return result;
+  }
+
+  template<typename U>
+  static BinaryTreeType<U> map(U(*f)(T const &), BinaryTreeType<T> const &t) {
+    return map(f, t.root());
+  }
+
+  static std::vector<std::vector<T> > levels(BinaryTreeType<T> const &tree) {
+    std::vector<std::vector<T> > result;
+    if (tree.empty()) return result;
+
+    std::queue<typename BinaryTreeType<T>::position> q;
+    q.push(tree.root());
+
+    while (!q.empty()) {
+      size_t levelSize = q.size();
+
+      std::vector<T> level;
+      for (size_t i = 0; i < levelSize; ++i) {
+        auto currPos = q.front(); q.pop();
+
+        // другият вариант беше да проверим дали
+        // currPos.left() и currPos.right() са валидни
+        // и да не ги слагаме в опашаката, ако не са
+        if (!currPos.valid()) continue;
+
+        level.push_back(*currPos);
+
+        q.push(currPos.left());
+        q.push(currPos.right());
+      }
+
+      result.push_back(level);
+    }
+    return result;
+  }
+
+  static std::vector<std::vector<T> > paths(BinaryTreeType<T> const &t) {
+    std::vector<std::vector<T> > result;
+    std::vector<T> acc;
+    paths(t.root(), acc, result);
+    return result;
   }
 
 private:
-  static int height(typename BinaryTreeType<T>::position pos) {
+  static long long height(position pos) {
     if (!pos.valid()) return -1;
-    if (!pos.left().valid() && !pos.right().valid()) return 0;
-    return 1 + std::max(height(pos.left()), height(pos.right()));
+    if (!pos.left().valid() && !pos.right().valid) return 0;
+
+    long long lh = height(pos.left());
+    long long rh = height(pos.right());
+    return 1 + std::max(lh, rh);
+  }
+
+  static long long diameter(position pos) {
+    if (!pos.valid()) return -1;
+    long long diam = height(pos.left()) + height(pos.right());
+    return 2 + std::max({diam, diameter(pos.left()), diameter(pos.right())});
+  }
+
+  static void serialise(position pos,
+    std::string const &prefix,
+    bool isLeft,
+    std::ostream& os) {
+
+    if(!pos.valid()) return;
+
+    os << prefix;
+
+    os << (isLeft ? "|---" : "+---" );
+
+    os << *pos << std::endl;
+
+    serialise(pos.left(), prefix + (isLeft ? "|   " : "    "), true, os);
+    serialise(pos.right(), prefix + (isLeft ? "|   " : "    "), false, os);
+  }
+
+  static void inorder(position pos, std::vector<T> &result) {
+    if (!pos.valid()) return;
+
+    inorder(pos.left(), result);
+    result.push_back(*pos);
+    inorder(pos.right(), result);
+  }
+
+  static void preorder(position pos, std::vector<T> &result) {
+    if (!pos.valid()) return;
+
+    result.push_back(*pos);
+    preorder(pos.left(), result);
+    preorder(pos.right(), result);
+  }
+
+  template<typename U>
+  static BinaryTreeType<U> map(U(*f)(T const &), position pos) {
+    if (!pos.valid()) return BinaryTreeType<U>{};
+    return BinaryTreeType<U>(f(*pos),
+                        map(f, pos.left()),
+                        map(f, pos.right()));
+  }
+
+  static void paths(position pos, std::vector<T> &acc, std::vector<std::vector<T> > &result) {
+    // тази проверка е важна :)
+    if (!pos.valid()) {
+      return;
+    }
+
+    acc.push_back(*pos);
+
+    if (!pos.left().valid() && !pos.right().valid()) {
+      result.push_back(acc);
+      acc.pop_back();
+      return;
+    }
+
+    paths(pos.left(), acc, result);
+    paths(pos.right(), acc, result);
   }
 
 };
